4f38b8e5c36e6fb31489f504a6b41c86ce398324
Mukul <mkltaneja@gmail.com>
2021-06-05 00:07:33 +0530

    Educational Round 110

---
+++ Educational_Round_#110/contest_1535/B_Array_Reodering.cpp
@@ -0,0 +1,165 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define f first
+#define s second
+#define ll long long int
+#define ull unsigned long long int
+#define pii pair<int, int>
+#define pib pair<int, bool>
+#define pbi pair<bool, int>
+#define pbb pair<bool, bool>
+#define pic pair<int, char>
+#define pci pair<char, int>
+#define pcc pair<char, char>
+#define pis pair<int, string>
+#define psi pair<string, int>
+#define pss pair<string, string>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vd vector<double>
+#define vvd vector<vd>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vull vector<ull>
+#define vvull vector<vull>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vc vector<char>
+#define vvc vector<vc>
+#define vs vector<string>
+#define vvs vector<vs>
+#define vpii vector<pii>
+#define vpbi vector<pbi>
+#define vpib vector<pib>
+#define vpbb vector<pbb>
+#define vpci vector<pci>
+#define vpic vector<pic>
+#define vpcc vector<pcc>
+#define vpsi vector<psi>
+#define vpis vector<pis>
+#define vpss vector<pss>
+#define um unordered_map
+#define umii unordered_map<int, int>
+#define umci unordered_map<char, int>
+#define umic unordered_map<int, char>
+#define umsi unordered_map<string, int>
+#define umis unordered_map<int, string>
+#define omii map<int, int>
+#define omci map<char, int>
+#define omic map<int, char>
+#define omsi map<string, int>
+#define omis map<int, string>
+#define lp(i, a, b) for (int i = a; i < b; i++)
+#define lpr(i, b, a) for (int i = b; i > a; i--)
+#define lpl(i, a, b) for (ll i = a; i < b; i++)
+#define lplr(i, b, a) for (ll i = b; i > a; i--)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define fastio(){ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);}
+#define mod 1000000007
+
+ll binpow(ll a, ll b)
+{
+    ll ans = 1;
+    while (b)
+    {
+        if (b & 1)
+            ans *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return ans;
+}
+ll binpowmod(ll a, ll b)
+{
+    ll ans = 1;
+    while (b)
+    {
+        if (b & 1)
+            ans = (ans * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return ans;
+}
+
+bool ispal(string &a)
+{
+    for (int i = 0; i < a.size() / 2; i++)
+        if (a[i] != a[a.size() - i - 1])
+            return false;
+    return true;
+}
+
+void display(int n, vi &a)
+{
+    for(int x : a) cout<<x<<" ";
+    cout<<"\n";
+}
+void display(int n, int m, vvi &a)
+{
+    lp(i, 0, n)
+    {
+        lp(j, 0, m)
+            cout<<a[i][j]<<" ";
+        cout<<"\n";
+    }
+    cout<<"\n";
+}
+
+//////////////////////////////////////////////////////////////////MUKUL TANEJA///////////////////////////////////////////////////
+
+void solve(int n, vi &a)
+{
+    int ans = 0;
+    vi x(n);
+    int j = 0;
+    for(int i = 0; i < n; i++)
+        if(a[i] % 2 == 0)
+            x[j++] = a[i];
+    int k = j;
+    for(int i = 0; i < n; i++)
+        if(a[i] % 2)
+            x[j++] = a[i];
+    sort(x.begin(), x.begin()+k, greater<int>());
+    sort(x.begin()+k, x.end(), greater<int>());
+    // for(int i = 0; i < k; i++)
+    //     cout<<x[i]<<" ";
+    // cout<<endl;
+    // for(int i = k; i < n; i++)
+    //     cout<<x[i]<<" ";
+    // cout<<endl;
+    // cout<<k<<endl;
+    for(int i = 0; i < k; i++)
+        for(int j = i+1; j < k; j++)
+            if(__gcd(x[i], 2*x[j]) > 1) 
+                ans++;
+    for(int i = k; i < n; i++)
+        for(int j = i+1; j < n; j++)
+            if(__gcd(x[i], 2*x[j]) > 1) 
+                ans++;
+    ans += (k)*(n-k);
+    cout<<ans<<"\n";
+}
+
+int main()
+{
+    fastio();
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vi a(n);
+        lp(i, 0, n)
+        {
+            cin >> a[i];
+            // cout<<a[i]<<" ";
+        }
+
+        solve(n, a);
+    }
+    return 0;
+}
---
+++ Educational_Round_#110/contest_1535/C_Unstable_String.cpp
@@ -0,0 +1,164 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define f first
+#define s second
+#define ll long long int
+#define ull unsigned long long int
+#define pii pair<int, int>
+#define pib pair<int, bool>
+#define pbi pair<bool, int>
+#define pbb pair<bool, bool>
+#define pic pair<int, char>
+#define pci pair<char, int>
+#define pcc pair<char, char>
+#define pis pair<int, string>
+#define psi pair<string, int>
+#define pss pair<string, string>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vd vector<double>
+#define vvd vector<vd>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vull vector<ull>
+#define vvull vector<vull>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vc vector<char>
+#define vvc vector<vc>
+#define vs vector<string>
+#define vvs vector<vs>
+#define vpii vector<pii>
+#define vpbi vector<pbi>
+#define vpib vector<pib>
+#define vpbb vector<pbb>
+#define vpci vector<pci>
+#define vpic vector<pic>
+#define vpcc vector<pcc>
+#define vpsi vector<psi>
+#define vpis vector<pis>
+#define vpss vector<pss>
+#define um unordered_map
+#define umii unordered_map<int, int>
+#define umci unordered_map<char, int>
+#define umic unordered_map<int, char>
+#define umsi unordered_map<string, int>
+#define umis unordered_map<int, string>
+#define omii map<int, int>
+#define omci map<char, int>
+#define omic map<int, char>
+#define omsi map<string, int>
+#define omis map<int, string>
+#define lp(i, a, b) for (int i = a; i < b; i++)
+#define lpr(i, b, a) for (int i = b; i > a; i--)
+#define lpl(i, a, b) for (ll i = a; i < b; i++)
+#define lplr(i, b, a) for (ll i = b; i > a; i--)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define fastio(){ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);}
+#define mod 1000000007
+
+ll binpow(ll a, ll b)
+{
+    ll ans = 1;
+    while (b)
+    {
+        if (b & 1)
+            ans *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return ans;
+}
+ll binpowmod(ll a, ll b)
+{
+    ll ans = 1;
+    while (b)
+    {
+        if (b & 1)
+            ans = (ans * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return ans;
+}
+
+bool ispal(string &a)
+{
+    for (int i = 0; i < a.size() / 2; i++)
+        if (a[i] != a[a.size() - i - 1])
+            return false;
+    return true;
+}
+
+void display(int n, vi &a)
+{
+    for(int x : a) cout<<x<<" ";
+    cout<<"\n";
+}
+void display(int n, int m, vvi &a)
+{
+    lp(i, 0, n)
+    {
+        lp(j, 0, m)
+            cout<<a[i][j]<<" ";
+        cout<<"\n";
+    }
+    cout<<"\n";
+}
+
+//////////////////////////////////////////////////////////////////MUKUL TANEJA///////////////////////////////////////////////////
+
+void solve(string &a)
+{
+    int x = 0;
+    ull ans = 0;
+    int j = 0, k = 0;
+    bool firstq = true;
+    for(int i = 0; i < a.size(); i++)
+    {
+        if(a[i] == '?' || a[i]-'0' == x)
+        {
+            ans += (i-j+1);
+            if(a[i] == '?' && firstq)
+            {
+                firstq = false;
+                k = i;
+            }
+            else if(a[i] != '?') 
+            {
+                firstq = true;
+                k = i+1;
+            }
+            x ^= 1;
+        }
+        else 
+        {
+            // cout<<i<<", "<<k<<": "<<endl;
+            j = k;
+            k = i+1;
+            firstq = true;
+            x = ((a[i]-'0')+1)%2;
+            // cout<<x<<endl;
+            ans += (i-j+1);
+        }
+        // x ^= 1;
+    }
+    cout<<ans<<"\n";
+}
+
+int main()
+{
+    fastio();
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string a;
+        cin>>a;
+
+        solve(a);
+    }
+    return 0;
+}
